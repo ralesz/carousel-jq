2c608317bc28f09d10b6462efe38825a6ba01bee
Rafal Leszczynski <info@grafikart.pl>
2018-09-25 22:08:45 +0200

    carousel02

--- js/script.js
+++ js/script.js
@@ -1,9 +1,14 @@
+'use strict';
 $(function() {
-  let currentSlide = 0;
+  
+  let currentSlide = 0; //bierzący slajd
   $("#dot_" + currentSlide).addClass("active");
   var carouselList = $('#carousel .photo');
-  const time = setInterval(changeSlide, 3000);
+  var timeSlide;
+  setTime();
+  
 
+  //funkcja przesuwania slajdu w prawo  
   function changeSlide () {
     carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide);
     
@@ -11,24 +16,15 @@ $(function() {
       currentSlide = 0;
       $(".pagination li").removeClass("active");
       $("#dot_" + currentSlide).addClass("active");
-      console.log(currentSlide); 
       } 
       else {
         currentSlide++;
         $(".pagination li").removeClass("active");
         $("#dot_" + currentSlide).addClass("active");
-        console.log(currentSlide); 
       }
-  
-    function moveFirstSlide() {
-      var firstItem = carouselList.find('li:first');
-      var lastItem = carouselList.find('li:last');
-      lastItem.after(firstItem);
-      carouselList.css({marginLeft:0});
-    }
   }
 
-
+  // funkcja przesuwania slajdu w lewo  
   function changeSlideBack() {
     moveEndSlide();
     carouselList.animate({'marginLeft':0}, 500);
@@ -37,56 +33,77 @@ $(function() {
         currentSlide = 4;
         $(".pagination li").removeClass("active");
         $("#dot_" + currentSlide).addClass("active");
-        console.log(currentSlide);  
       } 
       else {
         currentSlide--;
         $(".pagination li").removeClass("active");
         $("#dot_" + currentSlide).addClass("active");
-        console.log(currentSlide);
-      }
-      
-      function moveEndSlide () {
-        var firstItem = carouselList.find("li:first");
-        var lastItem = carouselList.find("li:last");
-        firstItem.before(lastItem);
-        carouselList.css({marginLeft:-400});
       }
   };
 
-  function activepoint () {
-
-    // var point = $(".pagination .dot");
-    // point.on('click', '.dot', function() {
-
-    $(".pagination .dot").on('click', function(){
-    var point = $(this).index();
-    console.log(point);
-    $(".pagination .dot").removeClass("active");
-    $("#dot_" + point).addClass("active");
-    carouselList.animate({'marginLeft': -400 * point}, 100);
-    currentSlide = point;
-    });	
+  //  dodanie slajdu pierwszego na koncu
+  function moveFirstSlide() {
+    var firstItem = carouselList.find('li:first');
+    var lastItem = carouselList.find('li:last');
+    lastItem.after(firstItem);
+    carouselList.css({marginLeft:0});
   }
 
+  // funkcja dodania slajdu ostatniego przed pierwszym
+  function moveEndSlide () {
+    var firstItem = carouselList.find("li:first");
+    var lastItem = carouselList.find("li:last");
+    firstItem.before(lastItem);
+    carouselList.css({marginLeft:-400});
+  }
+  
+  //funkcja animacji
+  function anim_slide (point) {
+    carouselList.animate({'marginLeft': -400 * point}, 500);
+  }
 
-    var active = $(".dot");
-    active.click(function () {
-      clearInterval(time);
-      activepoint();
-    });
+  //czekaj na kliknięcie paginacji
+  $(".pagination .dot").on('click', dots);
+  
+  //funkcja wyboru zdjecia z dolnej paginacji
+  function dots () {
+  var point = $(this).index();
+  console.log(point);
+  $(".pagination .dot").removeClass("active");
+  $("#dot_" + point).addClass("active");
+  clearInterval(timeSlide);
+  anim_slide (point);
+  setTime();
+  currentSlide == point;
+};
 
 
+// funkcja ustawienia setintervalu    
+  function setTime() {
+  timeSlide = setInterval(changeSlide, 3000)
+  };  
+
+// strzałka w prawo  
   var nextSlide = $("#btn-next");
   nextSlide.click(function () {
-    clearInterval(time);
+    clearInterval(timeSlide);
     changeSlide();
+    setTime();
   });
 
+  // strzałka w lewo
   var prevSlide = $("#btn-prev");
   prevSlide.click(function () {
-    clearInterval(time);
+    clearInterval(timeSlide);
     changeSlideBack();
+    setTime();
   });
 
 });
+
+
+
+
+
+
+
